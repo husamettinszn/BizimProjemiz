ea61a28c19abed90cf4c8182f6dcd8bbb7fe673f
husamettinszn <husamettinszn@gmail.com>
2020-11-19 16:45:29 +0100

    mesaj

--- src/BizimProje.java
+++ src/BizimProje.java
@@ -4,6 +4,7 @@ public class BizimProje {
 	public static void main(String[] args) {
 		System.out.println("16:36 bizim proje");
 		System.out.println("16:39 bizim projemiz");
+		System.out.println("16:44 b");
 
 	}
 
